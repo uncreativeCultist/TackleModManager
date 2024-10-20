dda60ab3194b971de11d4f8fbc81b3faf43c970e
uncreativeCultist <uncreativeCultist@lainiwakura.xyz>
2024-10-20 16:05:39 -0400

    Fix issue with game being installed on C: drive (again? really?)

--- Tackle-src.bat
+++ Tackle-src.bat
@@ -1,5 +1,7 @@
 @echo off
-set "currentver=2.0.0"
+::Shrink window in production builds!
+::mode con: cols=65 lines=10
+set "currentver=2.0.1"
 TITLE Tackle Debug Console
 echo checking version
 curl -sL -o C:\Users\Public\Downloads\ver.txt https://raw.githubusercontent.com/uncreativeCultist/TackleModManager/refs/heads/main/Storage/ver.txt >nul
@@ -19,7 +21,7 @@ if exist "C:\Program Files (x86)\Steam\steamapps\common\WEBFISHING\" (
   mklink /j "C:\Users\Public\Downloads\WEBFISHINGLINK" "C:\Program Files (x86)\Steam\steamapps\common\WEBFISHING"
   echo DEBUG: creasting symlink because tar is stupid and can't handle spaces apparently
   set "WFDIR=C:\Users\Public\Downloads\WEBFISHINGLINK"
-  goto main
+  goto verifypath
 ) else (
   goto setup
 )
@@ -32,16 +34,10 @@ if exist "C:\Users\Public\Downloads\bait.txt" (
   set /p WFDIR=<C:\Users\Public\Downloads\bait.txt
   goto verifypath
 ) else (
-  ::cls
   echo DEBUG: nope, we don't. ask em for a location please
   powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Please enter your WEBFISHING path! Please make sure it starts with the drive letter, does NOT end with \', 'Tackle Mod Manager v%currentver% - @uncreativecultist')}" > C:\Users\Public\Downloads\bait.txt
-  ::set /p WFDIR=
   echo DEBUG: cool, save that location for later please
   set /p WFDIR=<C:\Users\Public\Downloads\bait.txt
-  ::echo Tackle couldn't find WEBFISHING installed on your computer!
-  ::echo Please enter your WEBFISHING path! 
-  ::set /p WFDIR=
-  ::>C:\Users\Public\Downloads\bait.txt echo %WFDIR%
   goto verifypath
 )
 
@@ -49,14 +45,11 @@ if exist "C:\Users\Public\Downloads\bait.txt" (
 if exist "%WFDIR%\webfishing.exe" (
   goto verifyweave
 ) else (
-  ::cls
   echo DEBUG: hmm, cant find webfishing here.
   goto manualsetup
 )
 
 :verifyweave
-::leaving this here for now
-::https://github.com/NotNite/GDWeave/releases/latest/download/GDWeave.zip
 if exist "%WFDIR%\GDWeave\" (
   goto main
 ) else (
@@ -72,25 +65,14 @@ if exist "%WFDIR%\GDWeave\" (
   del /q C:\Users\Public\Downloads\GDWeave.zip
   goto verifyweave
 )
-  ::cls
-  ::echo It looks like you don't have GDWeave installed!
-  ::echo Currently, Tackle doesn't have the functionality to automatically install GDWeave :(
-  ::echo Please follow the instructions on the Github page!
-  ::timeout /t 5
-  ::start https://github.com/NotNite/GDWeave?tab=readme-ov-file#installation
-  ::exit
 
 
 :manualsetup
-::echo Please enter your WEBFISHING path! 
-::echo Please make sure it starts with the drive letter, does NOT end with \
 powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Please enter your WEBFISHING path! Please make sure it starts with the drive letter, does NOT end with \', 'Tackle Mod Manager v%currentver% - @uncreativecultist')}" > C:\Users\Public\Downloads\bait.txt
-::set /p WFDIR=
 set /p WFDIR=<C:\Users\Public\Downloads\bait.txt
 goto verifypath
 
 :main
-::cls
 IF [%1] EQU [] Goto:error
 
 if exist "%WFDIR%\GDWeave\mods" (
@@ -105,10 +87,8 @@ cd "%WFDIR%\GDWeave\mods\"
 tar -xf "%~1" -C %WFDIR%\GDWeave\mods
 echo DEBUG: Mod has been installed
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'Tackle Mod Manager v%currentver% - @uncreativecultist', 'Mod has been installed!', [System.Windows.Forms.ToolTipIcon]::None)}"
-::timeout /t 5
 exit
 
 :error
 echo DEBUG: Incorrect usage, whoopsie daisy!
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Please drag and drop a mod file onto the bat file to continue!', 'Tackle Mod Manager v%currentver% - @uncreativecultist', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
-::timeout /t 5
